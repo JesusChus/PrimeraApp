80936898be506a57ba0a1b1d07c89670a4c1d434
Jesus David <dpajesus1@gmail.com>
2024-03-11 14:59:31 +0100

    primer commit

---
+++ app/src/main/java/com/chusdevil/botom_toast/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.chusdevil.botom_toast
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var boton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        iniciar()
+    }
+
+    private fun iniciar() {
+        boton = findViewById(R.id.btToast)
+        boton.setOnClickListener { Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show() }
+
+    }
+
+}
+
