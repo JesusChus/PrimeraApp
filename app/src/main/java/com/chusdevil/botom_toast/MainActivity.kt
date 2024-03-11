72380d2517adfe0e411be0786f31baf3166326f1
Jesus David <dpajesus1@gmail.com>
2024-03-11 15:02:45 +0100

    modifique el main

--- app/src/main/java/com/chusdevil/botom_toast/MainActivity.kt
+++ app/src/main/java/com/chusdevil/botom_toast/MainActivity.kt
@@ -16,14 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         iniciar()
     }
